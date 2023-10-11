b855dd8f9bb487587cb1a6cb42d3b3134c4901ab
daiyunzhou <daiyunz@chanjet.com>
2023-10-11 12:51:30 +0800

    rebuild

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'dumi';
 
 //github仓库名称
 // const defaultPath = '/fe-interview';
-const defaultPath = '/';
+const defaultPath = '';
 //打包后gh-pages默认会拼接仓库名称在路径上
 const baseUrl = process.env.NODE_ENV === 'production' ? defaultPath : '';
 export default defineConfig({
